314a922a9878e2b3523f97affda12705070c3fbe
SimoneSpirito <simospiri01@gmail.com>
2022-02-04 19:15:31 +0100

    BONUS - style with scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,36 @@
 
-<ul>
-    @foreach ($movies as $movie)
-        <li>{{$movie->title}}</li>
-    @endforeach
-</ul>
+@extends('layouts.base')
+
+@section('content')
+
+<main>
+    <div class="container">
+        <div class="movies">
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <img src="{{$movie->poster}}" alt="{{$movie->title}}">
+                    <ul class="info">
+                        <li class="title">
+                           {{$movie->title}} ({{$movie->anno}})
+                        </li>
+                        <li class="genere">
+                          <span>Generi:</span> {{$movie->genere}}
+                        </li>
+                        <li class="regista">
+                            <span>Regista:</span> {{$movie->regista}}
+                        </li>
+                        <li class="durata">
+                            <span>Durata:</span> {{$movie->durata}}min
+                        </li>
+                        <li class="trama">
+                            <span>Descrizione:</span> {{$movie->trama}}
+                        </li>
+                    </ul>
+                    <div class="pop-corn"><span>🍿</span>{{$movie->vote}}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</main>
+    
+@endsection
